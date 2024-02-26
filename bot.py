ed8f9c4ad60903d61ea8caa6abd1efebb0d2732e
dyjf97 <dyjf97@gmail.com>
2024-02-27 02:06:38 +0800

    added auto send message every one second feature

--- bot.py
+++ bot.py
@@ -10,9 +10,25 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+@tasks.loop(seconds=1)
+async def send_message(ctx):
+    await ctx.channel.send("This is an auto-sent message!")
+
 @bot.event
 async def on_ready():
     print('Bot is ready.')
+
+@bot.command()
+async def auto_message_start(ctx):
+    send_message.start(ctx)
+    await ctx.send("Auto-send message function started.")
+
+@bot.command()
+async def auto_message_stop(ctx):
+    send_message.stop()
+    await ctx.send("Auto-send message function stopped.")
+
+
 
 load_dotenv()
 bot.run(os.getenv("TOKEN"))
